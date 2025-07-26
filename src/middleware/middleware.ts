a8d1e414a74d5be94958815d5b0394a510a6cc07
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-07-27 04:00:34 +0700

    fix: authentication

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,28 +1,42 @@
-// middleware.ts
+// // middleware.ts
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// // Halaman yang perlu proteksi login
+// const protectedRoutes = [
+//   "/admin",
+//   "/admin/halaman-desa",
+//   "/admin/kependudukan",
+// ];
+
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get("token")?.value;
+
+//   if (
+//     protectedRoutes.some((path) => request.nextUrl.pathname.startsWith(path))
+//   ) {
+//     if (!token) {
+//       return NextResponse.redirect(new URL("/login", request.url));
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/admin/:path*", "/admin"],
+// };
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
-// Halaman yang perlu proteksi login
-const protectedRoutes = [
-  "/admin",
-  "/admin/halaman-desa",
-  "/admin/kependudukan",
-];
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  if (
-    protectedRoutes.some((path) => request.nextUrl.pathname.startsWith(path))
-  ) {
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  // Jika belum login dan akses ke /admin/*
+  if (!token && request.nextUrl.pathname.startsWith("/admin")) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/admin/:path*", "/admin"],
-};
